ae4dbe2416f814703bf3449e9441e84bc3d60996
tom91011 <tomnightingale@hotmail.com>
2022-05-11 15:44:32 +0100

    auto populate table based on JSON gameData, currently hardcoded data

--- src/app/container/table/table.component.ts
+++ src/app/container/table/table.component.ts
@@ -14,6 +14,38 @@ interface Player {
 export class TableComponent implements OnInit {
   players: Player[] = playersData
 
+  gameData = [
+    {
+      "PlayerId": 5,
+      "TotalScore": 410,
+      "GamesPlayed": 16
+    }, {
+      "PlayerId": 2,
+      "TotalScore": 640,
+      "GamesPlayed": 15
+    }, {
+      "PlayerId": 6,
+      "TotalScore": 200,
+      "GamesPlayed": 9
+    }, {
+      "PlayerId": 1,
+      "TotalScore": 700,
+      "GamesPlayed": 9
+    }, {
+      "PlayerId": 4,
+      "TotalScore": 150,
+      "GamesPlayed": 9
+    }, {
+      "PlayerId": 3,
+      "TotalScore": 50,
+      "GamesPlayed": 3
+    }, {
+      "PlayerId": 6,
+      "TotalScore": 300,
+      "GamesPlayed": 8
+    }
+  ]
+  
   constructor() {
    }
 
