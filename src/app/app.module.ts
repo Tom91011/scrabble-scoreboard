0af7a6b356eaaea0b307e588fbdd6ba11058b208
tom91011 <tomnightingale@hotmail.com>
2022-05-11 13:37:32 +0100

    add basic html structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
+import { TableComponent } from './container/table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
+    TableComponent,
   ],
   imports: [
     BrowserModule
--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -11,5 +11,4 @@ export class ContainerComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
